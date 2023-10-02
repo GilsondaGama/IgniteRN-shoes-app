66d37cd14ae1294cfbf34d81cecfdbd15231356c
Gilson da Gama <gilsondagama@hotmail.com>
2023-10-02 02:35:24 -0300

    Instalando o OneSignal no projeto

--- App.tsx
+++ App.tsx
@@ -1,26 +1,32 @@
+import { useState } from "react";
 import { StatusBar } from "react-native";
-
-import OneSignal from "react-native-onesignal";
-
 import { NativeBaseProvider } from "native-base";
+
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
-import { Routes } from "./src/routes";
 
 import { THEME } from "./src/theme";
+import { Routes } from "./src/routes";
 import { Loading } from "./src/components/Loading";
-
 import { CartContextProvider } from "./src/contexts/CartContext";
+
+import { Notification } from "./src/components/Notification";
+import { tagUserEmailCreate } from "./src/notifications/notificationsTags";
+
+import OneSignal, { NotificationReceivedEvent, OSNotification } from "react-native-onesignal";
 
 const { ONESIGNAL_APP_ID } = process.env;
 OneSignal.setAppId(ONESIGNAL_APP_ID!);
 
-export default function App() {
+export default function App() { 
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const [notification, setNotification] = useState<OSNotification | null>(null);
+
+  tagUserEmailCreate("gilsondagama@hotmail.com");
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -32,6 +38,8 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
+
+      <Notification title="mensagem de Notificação" onClose={() => {}}/>
     </NativeBaseProvider>
   );
 }
